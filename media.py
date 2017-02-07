ed933fce9b9330f5a9bda1df588f296a821876b0
hari krisha <hari.k.krishna4@gmail.com>
2017-02-07 14:33:44 -0600

    added comments and formatted the code

--- media.py
+++ media.py
@@ -1,15 +1,17 @@
 import webbrowser
 
+
 class Movie():
     """This class provides a way to store movie  related information"""
-    VALID_RATINGS =["G", "PG", "PG-13", "R"]
- 
-    def __init__ (self,movie_title,movie_storyline,poster_image,trailer_youtube):
-        self.title = movie_title
-        self.storyLine= movie_storyline
-        self.poster_image_url=  poster_image
-        self.trailer_youtube_url= trailer_youtube
+    VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
+    # function __init__ Creates Instance for each movie
+    def __init__ (self, movie_title, movie_storyline, poster_image, trailer_youtube):
+        self.title = movie_title
+        self.storyLine = movie_storyline
+        self.poster_image_url =  poster_image
+        self.trailer_youtube_url = trailer_youtube
+
+    # function show_trailer()  will use variable(self.trailer_youtube_url) from __init__ and plays youtube trailer
     def show_trailer(self):
         webbrowser.open( self.trailer_youtube_url)
-
